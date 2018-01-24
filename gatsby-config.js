f14eee8f2ae0af23c968529094543ee3c55a892a
Amberley Romo <amberley@stream.me>
2018-01-23 23:32:13 -0600

    Initial setup pulling content from contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACEID,
+        accessToken: process.env.CONTENTFUL_ACCESSTOKEN,
+      },
+    },
+  ],
 };
